f0384294de76bede955e24dd89a11d0d620e4539
Niclas do <niclasdoepner1@web.de>
2021-03-26 18:55:34 +0100

    Changed Editquiz view, removed info, added question overview component

--- my-app/src/components/QuizComponents/EditQuiz/EditQuizComponents.js
+++ my-app/src/components/QuizComponents/EditQuiz/EditQuizComponents.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { TextField, Button } from "@material-ui/core";
-export default function EditQuizContainer({ children }) {
+export default function EditQuizComponents({ children }) {
   return (
     <div className="EditQuizContainer">
       <div className="qc_textField">
@@ -10,15 +10,8 @@ export default function EditQuizContainer({ children }) {
           variant="filled"
         ></TextField>
       </div>
-      <div className="qc_info">
-        <span>Questions: 20</span>
-      </div>
-      <div className="qc_editQuestionDiv">
-        <Button id="qc_editQuestion" variant="outlined" color="primary">
-          Edit Question
-        </Button>
-      </div>
-
+      <EditQuizComponents.QuestionList></EditQuizComponents.QuestionList>
+      <EditQuizComponents.EditButton></EditQuizComponents.EditButton>
       <div className="qc_submitButton">
         <Button variant="contained" color="primary">
           Submit
@@ -27,3 +20,43 @@ export default function EditQuizContainer({ children }) {
     </div>
   );
 }
+
+/*
+ <div className="qc_info">
+        <span>Questions: 20</span>
+      </div>
+      */
+
+EditQuizComponents.EditButton = function EditQuizComponentsEditButton() {
+  return (
+    <div className="qc_editQuestionDiv">
+      <Button id="qc_editQuestion" variant="outlined" color="primary">
+        ADD
+      </Button>
+    </div>
+  );
+};
+
+EditQuizComponents.QuestionList = function EditQuizComponentsQuestionList({
+  questions = [],
+}) {
+  return (
+    <div className="qc_QuestionList">
+      {getEmptyPlaceholder()}
+      <div className="qc_singleQuestion">
+        <span>
+          Frage nr 1 ist gar keine so lange frage, sollte aber trotzdem
+          reinpassen fgalls noch längere kommen
+        </span>
+      </div>
+    </div>
+  );
+
+  function getEmptyPlaceholder() {
+    return (
+      <div className="qc_emptyQuestionList">
+        <span>No Questions added</span>
+      </div>
+    );
+  }
+};
